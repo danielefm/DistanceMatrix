883313fef4b16b02d8994c1ae415d173f58fff7a
danielefm <danielefirme@gmail.com>
2020-04-11 19:13:59 -0300

    PEP-8 Style + new file names

--- distanceMatrix.py
+++ distanceMatrix.py
@@ -1,33 +1,48 @@
-import googlemaps
-import numpy as np
-import ogr, gdal
-import pandas as pd
-from random import randint
 import datetime
 import os
+from random import randint
 
-nextWedYear= 2020
-nextWedMonth= 4
-nextWedDay= 8
+import gdal
+import googlemaps
+import numpy as np
+import ogr
+import pandas as pd
 
-shapeFile = "Macrozonas_DF_2016.shp"
 
-loc_csv1 = pd.read_csv("origins.csv",sep=';')
-loc_csv2 = pd.read_csv("destinations.csv",sep=';')
-origin=loc_csv1["origin"].tolist()
-destination=loc_csv2["destination"].tolist()
+GOOGLE_KEY = "insert API KEY"
 
-modes = ['driving','walking','bicycling','transit']
+# We will make requests to Google Maps API that use traffic information
+# because we need to know the travel duration on a specific time of a
+# typical weekday. However, Google Maps only allows us to make requests
+# considering future times. Therefore, here we have to specify year,
+# month and day of the next typical Wednesday.
+
+year_of_next_wednesday= 2020
+month_of_next_wednesday= 4
+day_of_next_wednesday= 8
+
+shp_file = "Brasilia_DF_2016.shp"
+
+origin = pd.read_csv("origins.csv", sep=';')["origin"].tolist()
+destination = pd.read_csv("destinations.csv", 
+                          sep=';')["destination"].tolist()
+
+modes = ['driving', 'walking', 'bicycling', 'transit']
+
 
 def randomPoint(file, region):
-    polygon_fn = file
 
+    """ Returns a random point P = (X, Y) that lies within the area of
+    a poligon defined by the feature name "region" that is parte of
+    "file", which is an ESRI Shapefile type document.
+    """
+    
     # Define pixel_size which equals distance betweens points
     pixel_size = 0.003
 
     # Open the data source and read in the extent
-    source_ds = ogr.Open(polygon_fn)
-    source_layer = source_ds.GetLayer()
+    source_shapefile = ogr.Open(file)
+    source_layer = source_shapefile.GetLayer()
     source_layer.SetAttributeFilter("RA_NOME='"+region+"'")
     x_min, x_max, y_min, y_max = source_layer.GetExtent()
 
@@ -69,35 +84,72 @@ def randomPoint(file, region):
     # Remove temporary files
     os.remove('temp.tif')
 
+    # Choose a random point
     i = randint(0,multipoint.GetGeometryCount()-1)
     point = multipoint.GetGeometryRef(i)
     coord = (point.GetY(),point.GetX())
 
     return coord
 
+
 def commuteTime(fromPlace, toPlace, how, year, month, day):
-    when = (datetime.date(year,month,day)-datetime.date(1970,1,1)).total_seconds()+28800
-    gmaps = googlemaps.Client(key="AIzaSyDX9GTh5B0jDjsZ0fVe_owr1vBRoO6Yblg")
-    result_matrix = gmaps.distance_matrix(origins=fromPlace,destinations=toPlace,mode=how,departure_time=when)
+    
+    """ Returns the duration of a trip between two points (fromPlace),
+    given a specific transportation mode (how: 'driving','walking',
+    'bicycling', 'transit'), at 8 a.m. of the given day.
+    Returns -1 if there is no route found between the points, using
+    the given mode.
+    """
+
+    # In order to make a Google Maps API request, we have to specify
+    # the departure time as seconds since midnight, January 1, 1970.
+    # Therefore, we use the datetime library to calculate the time
+    # difference between the desired request day and 01/01/1970.
+    # At the end, we sum up 28800 because our request is at 8 a.m.,
+    # and there are 28000 seconds from midnight to 8 a.m.
+    
+    when = (datetime.date(year,month,day) 
+           - datetime.date(1970,1,1)).total_seconds()
+           + 28800
+    
+    gmaps = googlemaps.Client(key=GOOGLE_KEY)
+    
+    result_matrix = gmaps.distance_matrix(origins = fromPlace,
+                                          destinations=toPlace,
+                                          mode=how,
+                                          departure_time=when)
+
     if result_matrix['rows'][0]['elements'][0]['status']=='OK':
+    
         return result_matrix['rows'][0]['elements'][0]['duration']['value']
+    
     else: return -1
 
+
 for mode in modes:
-    df = pd.DataFrame(index=origin,columns=destination)
+
+    df = pd.DataFrame(index=origin, columns=destination)
     for o in origin:
         for d in destination:
             sum=0
             counts=0
-            for i in [0,1,2]:
-                pointOrigin = randomPoint(shapeFile, o)
-                pointDestination = randomPoint(shapeFile, d)
-                duration=commuteTime(pointOrigin,pointDestination,mode,nextWedYear,nextWedMonth,nextWedDay)
+            for i in [0, 1, 2]:
+                pointOrigin = randomPoint(shp_file, o)
+                pointDestination = randomPoint(shp_file, d)
+                duration=commuteTime(pointOrigin,
+                                     pointDestination,
+                                     mode,
+                                     year_of_next_wednesday,
+                                     month_of_next_wednesday,
+                                     day_of_next_wednesday)
+
                 if duration!=-1:
                     sum=sum+duration
                     counts=counts+1
+
             if counts!=0:
                df.loc[o,d]=sum/counts
             else:
                 df.loc[o,d]=-1
+                
     df.to_csv("mode"+mode+".csv")
